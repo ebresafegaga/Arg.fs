3ca980e0139643a61bee03af29aa308be258c782
ebresafegaga <ebresafegaga@gmail.com>
2020-07-08 18:18:58 +0100

    License stuff.

--- src/Arg.fs
+++ src/Arg.fs
@@ -1,1 +1,8 @@
+(*
+    Arg.fs - Command Line Argument Parser.
+ 
+    Created by Oghenevwogaga Ebresafe on Wed 8/6/20
+    Copyright © 2020 Oghenevwogaga Ebresafe. All rights reserved.
+*)
+
 module Arg
